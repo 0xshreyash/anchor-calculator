5660d93de6fdc12f85fc78ab422d5f128a00aeee
shreyashpatodia <patodiashreyash32@gmail.com>
2021-09-15 17:31:44 +0530

    Rewrite for readability

--- main.py
+++ main.py
@@ -82,6 +82,14 @@ def fetch_aust_price_table():
     return aust_price_df
 
 
+@st.cache
+def fetch_liquidation_transfer_back():
+    liquidation_transfers = \
+        'https://api.flipsidecrypto.com/api/v2/queries/cb6294ce-985a-4f8a-b6d2-43ea342e6969/data/latest'
+    liquidation_transfer_dfs = pd.read_json(liquidation_transfers)
+    return liquidation_transfer_dfs
+
+
 def filter_deposits(deposits_df, depositor=None):
     if depositor:
         return deposits_df[deposits_df['DEPOSITOR'] == depositor]
@@ -118,9 +126,15 @@ def filter_mirror_collateral_withdraws(collateral_withdraws_df, withdrawer=None)
     return collateral_withdraws_df
 
 
+def filter_aust_transfer_back(aust_transfer_back_df, owner=None):
+    if owner:
+        return aust_transfer_back_df[aust_transfer_back_df['OWNER'] == owner]
+    return aust_transfer_back_df
+
+
 def find_closest_aust_pair(aust_price_df, other_df):
     combined_df = pd.merge_asof(
-        other_df,
+        other_df.sort_values('BLOCK_ID'),
         aust_price_df.sort_values('BLOCK_ID'),
         on='BLOCK_ID',
         direction='nearest'
@@ -141,193 +155,228 @@ def compute_value_of_aust(
 st.title('Anchor Calculator: The pieces of the puzzle')
 
 
-st.subheader('aUST Value Table')
-aust_price_df = fetch_aust_price_table()
-st.write(aust_price_df)
+def show_aust_value_table():
+    st.subheader('aUST Value Table')
+    aust_price_df = fetch_aust_price_table()
+    st.write(aust_price_df)
+    return aust_price_df
 
 
-st.subheader('Pick user')
-user_address = st.text_input('User Address', value='terra1san7x37tn4zs09ufazgtyxf9h0ht9gm4t6qcdd')
+def show_user_picker():
+    st.subheader('Pick user')
+    user_address = st.text_input('User Address', value='terra1san7x37tn4zs09ufazgtyxf9h0ht9gm4t6qcdd')
+    return user_address
 
 
-st.subheader('Deposits')
-deposits_df = fetch_deposits()
-user_deposits = filter_deposits(deposits_df, depositor=user_address)
-st.write(user_deposits)
-
-total_deposits_ust = user_deposits['DEPOSIT_AMOUNT_UST'].sum(axis=0)
-total_aust_minted = user_deposits['MINT_AMOUNT_AUST'].sum(axis=0)
-st.write('Total user deposits (UST):', total_deposits_ust)
-st.write('Total user mint (aUST):', total_aust_minted)
-
-
-st.subheader('Redemptions')
-redemptions_df = fetch_redemptions()
-user_redemptions = filter_redemptions(redemptions_df, redeemer=user_address)
-st.write(user_redemptions)
-
-total_ust_redeemed = user_redemptions['UST_REDEEMED'].sum(axis=0)
-total_aust_burnt = user_redemptions['AUST_BURNT'].sum(axis=0)
-st.write('Total user redemptions (UST): ', total_ust_redeemed)
-st.write('Total user aUST burnt: ', total_aust_burnt)
-
-st.subheader('aUST Transfers')
-transfers_df = fetch_aust_transfers()
-aust_transfers_sent = filter_aust_sent(transfers_df, sender=user_address)
-aust_transfers_received = filter_aust_received(transfers_df, receiver=user_address)
-
-st.write('Transfers sent')
-st.write(aust_transfers_sent)
+def show_user_deposits(user_address):
+    st.subheader('Deposits')
+    deposits_df = fetch_deposits()
+    user_deposits = filter_deposits(deposits_df, depositor=user_address)
+    st.write(user_deposits)
+    total_deposits_ust = user_deposits['DEPOSIT_AMOUNT_UST'].sum(axis=0)
+    total_aust_minted = user_deposits['MINT_AMOUNT_AUST'].sum(axis=0)
+    st.write('Total user deposits (UST):', total_deposits_ust)
+    st.write('Total user mint (aUST):', total_aust_minted)
+    return user_deposits
 
 
-st.write('Transfers sent with UST price')
-aust_transfers_sent = find_closest_aust_pair(aust_price_df, aust_transfers_sent)
-aust_transfers_sent = compute_value_of_aust(
-    aust_transfers_sent,
-    aust_amount_col='AUST_TRANSFERRED',
-    aust_value_col='AVERAGED_AUST_VALUE',
-    total_ust_value_col='UST_TRANSFERRED'
+def show_user_redemptions(user_address):
+    st.subheader('Redemptions')
+    redemptions_df = fetch_redemptions()
+    user_redemptions = filter_redemptions(redemptions_df, redeemer=user_address)
+    st.write(user_redemptions)
+
+    total_ust_redeemed = user_redemptions['UST_REDEEMED'].sum(axis=0)
+    total_aust_burnt = user_redemptions['AUST_BURNT'].sum(axis=0)
+    st.write('Total user redemptions (UST): ', total_ust_redeemed)
+    st.write('Total user aUST burnt: ', total_aust_burnt)
+    return user_redemptions
+
+
+def show_user_aust_transfers(user_address, aust_price_df):
+    '''
+    Note: This function only accounts for aUST transfers performed by the user
+    '''
+    st.subheader('aUST Transfers')
+    transfers_df = fetch_aust_transfers()
+    aust_transfers_sent = filter_aust_sent(transfers_df, sender=user_address)
+    aust_transfers_received = filter_aust_received(transfers_df, receiver=user_address)
+
+    #############################################################################################
+
+    st.write('Transfers sent')
+
+    aust_transfers_sent = find_closest_aust_pair(aust_price_df, aust_transfers_sent)
+    aust_transfers_sent = compute_value_of_aust(
+        aust_transfers_sent,
+        aust_amount_col='AUST_TRANSFERRED',
+        aust_value_col='AVERAGED_AUST_VALUE',
+        total_ust_value_col='UST_TRANSFERRED'
+    )
+
+    st.write(aust_transfers_sent)
+
+    total_aust_transfers_sent = aust_transfers_sent['AUST_TRANSFERRED'].sum(axis=0)
+    redemptions_via_transfers = aust_transfers_sent['UST_TRANSFERRED'].sum(axis=0)
+    st.write('Total aUST transferred out: ', total_aust_transfers_sent)
+    st.write('Total redemption value via transfers out: ', redemptions_via_transfers)
+
+    #############################################################################################
+
+    st.write('Transfers received')
+
+    aust_transfers_received = find_closest_aust_pair(aust_price_df, aust_transfers_received)
+    aust_transfers_received = compute_value_of_aust(
+        aust_transfers_received,
+        aust_amount_col='AUST_TRANSFERRED',
+        aust_value_col='AVERAGED_AUST_VALUE',
+        total_ust_value_col='UST_TRANSFERRED'
+    )
+
+    st.write(aust_transfers_received)
+
+    total_aust_transfers_received = aust_transfers_received['AUST_TRANSFERRED'].sum(axis=0)
+    ust_deposits_via_transfers = aust_transfers_received['UST_TRANSFERRED'].sum(axis=0)
+    st.write('Total aUST transferred in: ', total_aust_transfers_received)
+    st.write('Total deposit value via transfers in: ', ust_deposits_via_transfers)
+
+    return aust_transfers_sent, aust_transfers_received
+
+
+def mirror_collateral_deposits(user_address):
+    st.subheader('Mirror aUST Collateral Deposits')
+    collateral_deposits_df = fetch_mirror_collateral_deposits()
+    user_collateral_deposits_df = filter_mirror_collateral_deposits(
+        collateral_deposits_df,
+        depositor=user_address
+    )
+    st.write(user_collateral_deposits_df)
+
+    total_aust_collateralised = user_collateral_deposits_df['AUST_COLLATERALISED'].sum(axis=0)
+    st.write('Total aUST Collateralised: ', total_aust_collateralised)
+    return user_collateral_deposits_df
+
+
+def mirror_collateral_withdrawn(user_address, aust_price_df):
+
+    st.subheader('Mirror aUST Collateral Withdraws')
+
+    collateral_withdraws_df = fetch_mirror_collateral_withdraws()
+    all_collateral_withdraws = filter_mirror_collateral_withdraws(
+        collateral_withdraws_df,
+        withdrawer=user_address
+    )
+    st.write('aUST Collateral Withdraws: ', all_collateral_withdraws)
+
+    user_collateral_withdraws_df = \
+        all_collateral_withdraws[
+            all_collateral_withdraws['POSITION_TYPE'] == 'Withdraw from Borrow'
+        ]
+
+    total_aust_withdrawn = user_collateral_withdraws_df['AUST_COLLATERALISED'].sum(axis=0)
+    st.write('Total aUST Withdrawn: ', total_aust_withdrawn)
+
+    aust_lost_to_fees = all_collateral_withdraws[
+        all_collateral_withdraws['POSITION_TYPE'] == 'Withdraw Protocol Fee'
+    ]
+    aust_lost_to_fees = find_closest_aust_pair(
+        aust_price_df=aust_price_df,
+        other_df=aust_lost_to_fees
+    )
+    aust_lost_to_fees = compute_value_of_aust(
+        aust_lost_to_fees,
+        aust_amount_col='AUST_COLLATERALISED',
+        aust_value_col='AVERAGED_AUST_VALUE',
+        total_ust_value_col='UST_VALUE_COLLATERALISED'
+    )
+
+    total_aust_lost_to_fees = aust_lost_to_fees['AUST_COLLATERALISED'].sum(axis=0)
+    st.write('Total aUST lost to protocol fees:', total_aust_lost_to_fees)
+
+    total_ust_value_lost_to_fees = aust_lost_to_fees['UST_VALUE_COLLATERALISED'].sum(axis=0)
+    st.write('UST value of aUST lost to protocol fees: ', total_ust_value_lost_to_fees)
+
+    aust_liquidated = all_collateral_withdraws[
+        all_collateral_withdraws['POSITION_TYPE'] == 'Liquidated aUST'
+    ]
+    aust_liquidated = find_closest_aust_pair(
+        aust_price_df=aust_price_df,
+        other_df=aust_liquidated
+    )
+    aust_liquidated = compute_value_of_aust(
+        aust_liquidated,
+        aust_amount_col='AUST_COLLATERALISED',
+        aust_value_col='AVERAGED_AUST_VALUE',
+        total_ust_value_col='UST_VALUE_COLLATERALISED'
+    )
+    total_aust_liquidated = aust_liquidated['AUST_COLLATERALISED'].sum(axis=0)
+    st.write('aUST lost to liquidations:', total_aust_liquidated)
+    total_ust_liquidated = \
+        aust_liquidated['UST_VALUE_COLLATERALISED'].sum(axis=0)
+    st.write('UST value lost to liquidations: ', total_ust_liquidated)
+
+    # Find all transfers back to a user after an aUST liquidation
+    aust_liquidation_transfer_back_df = fetch_liquidation_transfer_back()
+    user_liq_aust_transfer_back_df = filter_aust_transfer_back(
+        aust_liquidation_transfer_back_df, owner=user_address
+    )
+    st.write('User\'s aUST transferred back due to liquidation', user_liq_aust_transfer_back_df)
+    total_aust_transferred_back = user_liq_aust_transfer_back_df['AUST_TRANSFERRED_BACK'].sum(axis=0)
+    st.write('Total aUST transferred back to user from Mirror: ', total_aust_transferred_back)
+
+    return user_collateral_withdraws_df, aust_lost_to_fees, aust_liquidated, user_liq_aust_transfer_back_df
+
+aust_price_df = show_aust_value_table()
+user_address = show_user_picker()
+user_deposits_df = show_user_deposits(user_address)
+user_redemptions_df = show_user_redemptions(user_address)
+user_transfers_sent_df, user_transfers_received_df = show_user_aust_transfers(user_address, aust_price_df)
+user_collateral_deposits_df = mirror_collateral_deposits(user_address)
+user_collateral_withdraws_df, aust_lost_to_fees, aust_liquidated, aust_transferred_back = mirror_collateral_withdrawn(
+    user_address, aust_price_df
 )
-st.write(aust_transfers_sent)
 
-total_aust_transfers_sent = aust_transfers_sent['AUST_TRANSFERRED'].sum(axis=0)
-redemptions_via_transfers = aust_transfers_sent['UST_TRANSFERRED'].sum(axis=0)
-st.write('Total aUST transferred out: ', total_aust_transfers_sent)
-st.write('Total redemption value via transfers out: ', redemptions_via_transfers)
-
-
-st.write('Transfers received')
-st.write(aust_transfers_received)
-
-aust_transfers_received = find_closest_aust_pair(aust_price_df, aust_transfers_received)
-st.write('Transfers received with UST price')
-aust_transfers_received = compute_value_of_aust(
-    aust_transfers_received,
-    aust_amount_col='AUST_TRANSFERRED',
-    aust_value_col='AVERAGED_AUST_VALUE',
-    total_ust_value_col='UST_TRANSFERRED'
-)
-st.write(aust_transfers_received)
-
-total_aust_transfers_received = aust_transfers_received['AUST_TRANSFERRED'].sum(axis=0)
-ust_deposits_via_transfers = aust_transfers_received['UST_TRANSFERRED'].sum(axis=0)
-st.write('Total aUST transferred in: ', total_aust_transfers_received)
-st.write('Total deposit value via transfers in: ', ust_deposits_via_transfers)
-
-
-st.subheader('Mirror aUST Collateral Deposits')
-collateral_deposits_df = fetch_mirror_collateral_deposits()
-user_collateral_deposits_df = filter_mirror_collateral_deposits(
-    collateral_deposits_df,
-    depositor=user_address
-)
-st.write(user_collateral_deposits_df)
-
+total_aust_minted = user_deposits_df['MINT_AMOUNT_AUST'].sum(axis=0)
+total_aust_redeemed = user_redemptions_df['AUST_BURNT'].sum(axis=0)
 total_aust_collateralised = user_collateral_deposits_df['AUST_COLLATERALISED'].sum(axis=0)
-st.write('Total aUST Collateralised: ', total_aust_collateralised)
-
-
-st.subheader('Mirror aUST Collateral Withdraws')
-collateral_withdraws_df = fetch_mirror_collateral_withdraws()
-user_collateral_withdraws_df = filter_mirror_collateral_withdraws(
-    collateral_withdraws_df,
-    withdrawer=user_address
-)
-st.write(user_collateral_withdraws_df)
-
-total_aust_withdrawn = \
-    user_collateral_withdraws_df['AUST_COLLATERALISED'][
-        user_collateral_withdraws_df['POSITION_TYPE'] == 'Withdraw from Borrow'
-    ].sum(axis=0)
-st.write('Total aUST Withdrawn: ', total_aust_withdrawn)
-
-aust_lost_to_fees = user_collateral_withdraws_df[
-    user_collateral_withdraws_df['POSITION_TYPE'] == 'Withdraw Protocol Fee'
-]
-st.write(aust_lost_to_fees)
-aust_lost_to_fees = find_closest_aust_pair(
-    aust_price_df=aust_price_df,
-    other_df=aust_lost_to_fees
-)
-aust_lost_to_fees = compute_value_of_aust(
-    aust_lost_to_fees,
-    aust_amount_col='AUST_COLLATERALISED',
-    aust_value_col='AVERAGED_AUST_VALUE',
-    total_ust_value_col='UST_VALUE_COLLATERALISED'
-)
-
+total_aust_withdrawn = user_collateral_withdraws_df['AUST_COLLATERALISED'].sum(axis=0)
 total_aust_lost_to_fees = aust_lost_to_fees['AUST_COLLATERALISED'].sum(axis=0)
-st.write('aUST lost to protocol fees:', total_aust_lost_to_fees)
+total_aust_liquidated = aust_liquidated['AUST_COLLATERALISED'].sum(axis=0)
+total_aust_transferred_back_in_liq = aust_transferred_back['AUST_TRANSFERRED_BACK'].sum(axis=0)
+total_aust_transferred_in = user_transfers_received_df['AUST_TRANSFERRED'].sum(axis=0) - \
+                                user_transfers_sent_df['AUST_TRANSFERRED'].sum(axis=0)
 
-total_ust_value_lost_to_fees = aust_lost_to_fees['UST_VALUE_COLLATERALISED'].sum(axis=0)
-st.write('UST value of aUST lost to protocol fees: ', total_ust_value_lost_to_fees)
+total_aust_in_wallet = total_aust_minted - total_aust_redeemed - total_aust_collateralised + total_aust_withdrawn \
+                       + total_aust_transferred_back_in_liq + total_aust_transferred_in
+total_aust_on_mirror = total_aust_collateralised - total_aust_withdrawn - total_aust_transferred_back_in_liq \
+                       - total_aust_lost_to_fees - total_aust_liquidated
 
-aust_liquidated = user_collateral_withdraws_df[
-    user_collateral_withdraws_df['POSITION_TYPE'] == 'Liquidated aUST'
-]
-aust_liquidated = find_closest_aust_pair(
-    aust_price_df=aust_price_df,
-    other_df=aust_liquidated
-)
-aust_liquidated = compute_value_of_aust(
-    aust_liquidated,
-    aust_amount_col='AUST_COLLATERALISED',
-    aust_value_col='AVERAGED_AUST_VALUE',
-    total_ust_value_col='UST_VALUE_COLLATERALISED'
-)
-total_aust_liquidated = \
-    user_collateral_withdraws_df['AUST_COLLATERALISED'][
-        user_collateral_withdraws_df['POSITION_TYPE'] == 'Liquidated aUST'
-     ].sum(axis=0)
-st.write('aUST lost to liquidations:', total_aust_liquidated)
-total_ust_liquidated = \
-    aust_liquidated['UST_VALUE_COLLATERALISED'][
-        aust_liquidated['POSITION_TYPE'] == 'Liquidated aUST'
-        ].sum(axis=0)
-st.write('UST lost to liquidations: ', total_ust_liquidated)
+st.write('Total aUST in wallet: ', total_aust_in_wallet)
+st.write('Total aUST on Mirror: ', total_aust_on_mirror)
 
-net_aust_lost = total_aust_liquidated + total_aust_lost_to_fees
-net_aust_on_mirror = total_aust_collateralised - total_aust_withdrawn  - net_aust_lost
-st.write('Net aUST on Mirror', net_aust_on_mirror)
-
-st.subheader('Interest Calculation')
-
-total_deposits_ust = total_deposits_ust + ust_deposits_via_transfers
-st.write('Total Deposits UST: ', total_deposits_ust)
-total_ust_redeemed = total_ust_redeemed + redemptions_via_transfers
-st.write('Total Redemptions UST: ', total_ust_redeemed)
-
-st.write('Total aUST minted: ', total_aust_minted)
-st.write('Total aUST burnt: ', total_aust_burnt)
-st.write('aUST on Mirror: ', net_aust_on_mirror)
-
-net_aust_in_wallet = total_aust_minted - total_aust_burnt
-net_aust_in_wallet = net_aust_in_wallet - net_aust_on_mirror - net_aust_lost
-net_aust_in_wallet = net_aust_in_wallet - total_aust_transfers_sent + total_aust_transfers_received
-st.write('aUST currently in wallet: ', net_aust_in_wallet)
-
+total_aust_holdings = total_aust_in_wallet + total_aust_on_mirror
+st.write('Total aUST Holdings: ', total_aust_holdings)
 current_aust_price = float(
     aust_price_df[aust_price_df['BLOCK_ID'] == aust_price_df['BLOCK_ID'].max()]['AVERAGED_AUST_VALUE']
 )
-current_wallet_aust_value = current_aust_price * net_aust_in_wallet
-st.write('UST value of aUST in wallet: ', current_wallet_aust_value)
+total_aust_holding_value_ust = total_aust_holdings * current_aust_price
+st.write('Total UST value of aUST Holdings: ', total_aust_holding_value_ust)
 
-current_mirror_aust_value = current_aust_price * net_aust_on_mirror
-st.write('UST value of aUST in Mirror: ', current_mirror_aust_value)
-
-total_ust_value_of_aust_holdings = current_wallet_aust_value + current_mirror_aust_value
-st.write('UST value of all aUST holdings: ', total_ust_value_of_aust_holdings)
-
-interest_earned = total_ust_value_of_aust_holdings + total_ust_redeemed + \
-                  total_ust_value_lost_to_fees - total_deposits_ust
-interest_lost = total_ust_value_lost_to_fees + total_ust_liquidated
-st.write('Total interest earned: ', interest_earned)
-st.write('Value lost to liquidations + fees: ', interest_lost)
+total_ust_deposited = user_deposits_df['DEPOSIT_AMOUNT_UST'].sum(axis=0)
+total_ust_transferred_in = user_transfers_received_df['UST_TRANSFERRED'].sum(axis=0)
+total_ust_deposited += total_ust_transferred_in
 
 
+total_ust_redeemed = user_redemptions_df['UST_REDEEMED'].sum(axis=0)
+total_ust_value_transferred_out = user_transfers_sent_df['UST_TRANSFERRED'].sum(axis=0)
+total_ust_paid_in_fees = aust_lost_to_fees['UST_VALUE_COLLATERALISED'].sum(axis=0)
+total_ust_liquidated = aust_liquidated['UST_VALUE_COLLATERALISED'].sum(axis=0)
 
+total_ust_redeemed += total_ust_value_transferred_out
+total_ust_redeemed += total_ust_paid_in_fees
 
+interest_earned = total_aust_holding_value_ust - total_ust_deposited + total_ust_redeemed
+st.write('Total Interest Earned: ', interest_earned)
 
-
-
-
+ust_lost = total_ust_paid_in_fees + total_ust_liquidated
+st.write('Total UST lost: ', ust_lost)
